400621081dbaf35495c9590b0d67716bde70e7d1
Ross Kelso <rosskelso@btinternet.com>
2015-11-03 17:55:26 +0000

    purtified

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-	$('#five').click(function () {
+	$('#five').click(function (e) {
+		e.preventDefault();
 		$('#response').html(randNo(5));
 	});
-	$('#ten').click(function () {
+	$('#ten').click(function (e) {
+		e.preventDefault();
 		$('#response').html(randNo(10));
 	});
-	$('#fifteen').click(function () {
+	$('#fifteen').click(function (e) {
+		e.preventDefault();
 		$('#response').html(randNo(15));
 	});
 });
